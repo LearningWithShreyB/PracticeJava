65abf1f3b55f5c48ecaaed810a80bd7533fe00d8
ShreyB <shreypractice.github@gmail.com>
2026-03-09 19:18:39 +0530

    Second Commit

---
+++ src/day02/IncrementsDecrements.java
@@ -0,0 +1,42 @@
+package day02;
+
+public class IncrementsDecrements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Ex1
+		int a1 = 10;
+		int y1;
+		y1 = a1++;
+
+		System.out.println("Value of a1: " + a1);
+		System.out.println("Value of y1: " + y1);
+
+		// Ex2
+		int a2 = 20;
+		int y2;
+		y2 = ++a2;
+
+		System.out.println("Value of a2: " + a2);
+		System.out.println("Value of y2: " + y2);
+
+		// Ex1
+		int a3 = 30;
+		int y3;
+		y3 = a3--;
+
+		System.out.println("Value of a3: " + a3);
+		System.out.println("Value of y3: " + y3);
+
+		// Ex2
+		int a4 = 40;
+		int y4;
+		y4 = --a4;
+
+		System.out.println("Value of a4: " + a4);
+		System.out.println("Value of y4: " + y4);
+
+	}
+
+}
