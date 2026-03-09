f66c7adc357477bc3d9dbcede50087a8095283d5
ShreyB <shreypractice.github@gmail.com>
2026-03-09 18:47:53 +0530

    First Commit

---
+++ src/day01/HelloWorld.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class HelloWorld {
+	public static void main(String args[])
+	{
+		System.out.println("Hi There!!");
+		System.out.println("Welcome and Start journey with Java!!");
+	}
+
+}
