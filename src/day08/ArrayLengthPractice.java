164b2175067e622d8df176ceed1febc60d96e319
ShreyB <shreypractice.github@gmail.com>
2026-03-16 19:43:14 +0530

    Updates 1603_19:43

---
+++ src/day08/ArrayLengthPractice.java
@@ -0,0 +1,26 @@
+package day08;
+
+import java.util.Scanner;
+
+public class ArrayLengthPractice {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of an array: ");
+		int n = sc.nextInt();
+		System.out.println("Enter " + n + " elements:- ");
+		int[] a=new int[n];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println("The elements are: ");
+		for (int i = 0; i < n; i++) {
+			System.out.println(a[i]);
+		}
+
+		sc.close();
+
+	}
+
+}
