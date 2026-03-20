bf40193e0c16206d0b3660abf30c21940870edb4
ShreyB <shreypractice.github@gmail.com>
2026-03-20 17:58:54 +0530

    Updates 2003_18:00

---
+++ src/day10/ArraySort.java
@@ -0,0 +1,28 @@
+package day10;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of an array: ");
+		int n = sc.nextInt();
+		System.out.println("Enter " + n + " elements:- ");
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+
+		System.out.println("Elements before sorting: " + Arrays.toString(a));
+
+		Arrays.sort(a);
+
+		System.out.println("Elements after sorting: " + Arrays.toString(a));
+
+		sc.close();
+	}
+
+}
