6d69e5493ec77d78382b8b8d66611e6bebe0fc7c
ShreyB <shreypractice.github@gmail.com>
2026-03-20 13:20:26 +0530

    Up[dates 2003_13:20

---
+++ src/day10/SumOfElementsInArrayByUserInput.java
@@ -0,0 +1,28 @@
+package day10;
+
+import java.util.Scanner;
+
+public class SumOfElementsInArrayByUserInput {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of an array: ");
+		int n = sc.nextInt();
+		System.out.println("Enter " + n + " elements:- ");
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		int sum = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			sum = sum + a[i];
+		}
+
+		System.out.println("Sum of elements are: " + sum);
+		sc.close();
+
+	}
+
+}
---
+++ src/day10/TwoDArrayEnhancedForLoop.java
@@ -0,0 +1,18 @@
+package day10;
+
+public class TwoDArrayEnhancedForLoop {
+
+	public static void main(String[] args) {
+
+		int[][] a = { { 10, 20, 30, 40 }, { 50, 60 }, { 70, 80, 90 } };
+
+		for (int x[] : a) {
+			for (int y : x) {
+				System.out.print(y + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
