3e0fad4b59d254b6e307bd704f2f3755ba35122c
ShreyB <shreypractice.github@gmail.com>
2026-03-28 00:13:25 +0530

    Updates_2803_00:14

--- src/day16/ReportCardMain.java
+++ src/day16/ReportCardMain.java
@@ -8,9 +8,13 @@ public class ReportCardMain {
 
 		ReportCardPractice rcp = new ReportCardPractice();
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the student ID: ");
 		rcp.sid = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter the student name: ");
 		rcp.sname = sc.nextLine();
 
+		System.out.println("The details are: ");
 		rcp.result();
 
 		sc.close();
