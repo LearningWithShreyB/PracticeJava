dbb0ca5292c5210a2c822e4a70bc515c0dadee18
ShreyB <shreypractice.github@gmail.com>
2026-04-09 23:42:12 +0530

    Updates_0904_23:43

---
+++ src/day21/I1.java
@@ -0,0 +1,13 @@
+package day21;
+
+public interface I1 {
+
+	int i1 = 10;
+
+	void m1();
+
+	static void m2() {
+		System.out.println("This is a m2 method from I1 interface which is a static method.");
+	}
+
+}
