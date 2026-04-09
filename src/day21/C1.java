a5b9707f62d3167f2a16c2a4cfeccd0d8f6a0e02
ShreyB <shreypractice.github@gmail.com>
2026-04-09 23:51:37 +0530

    Updates_0904_23:52

---
+++ src/day21/C1.java
@@ -0,0 +1,21 @@
+package day21;
+
+public class C1 {
+
+	int c1 = 30;
+	static int c2 = 40;
+	final int c3 = 50;
+
+	public void m7() {
+		System.out.println("This is m7 method from parent class C1");
+	}
+
+	static void m8() {
+		System.out.println("This is m8 static method from parent class C1");
+	}
+
+	void m9() {
+		System.out.println("This is m9 method from parent class C1");
+	}
+
+}
