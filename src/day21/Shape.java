7987ac4cf84d68e58de75d415a43c3d47d4d7133
ShreyB <shreypractice.github@gmail.com>
2026-04-10 18:26:21 +0530

    Updates_1004_18:26

--- src/day21/Shape.java
+++ src/day21/Shape.java
@@ -1,5 +1,21 @@
 package day21;
 
-public class Shape {
+public abstract class Shape {
+
+	int color;
+
+	public abstract double area();
+
+	public Shape() {
+		System.out.println("This is a constructor from Shape class");
+	}
+
+	public Shape(int color) {
+		this.color = color;
+	}
+
+	public void m1() {
+		System.out.println("This is a normal method in Shape class");
+	}
 
 }
