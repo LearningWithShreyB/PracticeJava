3f7164e1565066049437496509b198279008a6d3
ShreyB <shreypractice.github@gmail.com>
2026-04-10 19:58:35 +0530

    Updates_1004_19:59

--- src/day21/MainChild.java
+++ src/day21/MainChild.java
@@ -7,6 +7,10 @@ public class MainChild {
 		Circle obj01 = new Circle(10, "Blue");
 
 		System.out.println("Area of the shape is: " + obj01.area());
+		
+		Shape obj02=new Circle(20, "Red");
+		
+		System.out.println("Area of the shape is: " + obj02.area());
 
 	}
 
