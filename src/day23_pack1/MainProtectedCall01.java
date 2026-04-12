11dbd64e8baa1272c795b26e21bb1cb5b111af6e
ShreyB <shreypractice.github@gmail.com>
2026-04-12 18:30:21 +0530

    Updates_1204_18:31

--- src/day23_pack1/MainProtectedCall01.java
+++ src/day23_pack1/MainProtectedCall01.java
@@ -2,14 +2,20 @@ package day23_pack1;
 
 import day23.ProtectedAccessModifierPractice;
 
-public class MainProtectedCall01 {
+public class MainProtectedCall01 extends ProtectedAccessModifierPractice {
 
 	public static void main(String[] args) {
 		ProtectedAccessModifierPractice o5 = new ProtectedAccessModifierPractice();
-		/* This is not accessible unless its inherited
-		System.out.println("The value of default variable: " + o5.f4);
-		o5.m5(200);*/
+		/*
+		 * This is not accessible unless its inherited
+		 * System.out.println("The value of default variable: " + o5.f4); o5.m5(200);
+		 */
 		o5.m6();
+
+		MainProtectedCall01 o6 = new MainProtectedCall01();
+		System.out.println("The value of protected variable: " + o6.f4);
+		o6.m5(400);
+		o6.m6();
 
 	}
 
