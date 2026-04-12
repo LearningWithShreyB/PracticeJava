75e2a34f02ab6628a9197be5794007223b1fefb8
ShreyB <shreypractice.github@gmail.com>
2026-04-12 17:52:19 +0530

    Updates_1204_17:53

---
+++ src/day23/PrivateAccessModifierPractice.java
@@ -0,0 +1,17 @@
+package day23;
+
+public class PrivateAccessModifierPractice {
+
+	private int f1 = 100;
+
+	private void m1() {
+		System.out.println("This is a private method which holds one value: " + f1);
+	}
+
+	void m2() {
+		System.out.println(
+				"This is a method after which I call private method which cant be access directly outside class.");
+		m1();
+	}
+
+}
