60a72a3a27a29d448352b87e962d992a3cd53d04
ShreyB <shreypractice.github@gmail.com>
2026-04-15 22:39:14 +0530

    Updates_1504_22:40

--- src/day24/Child01.java
+++ src/day24/Child01.java
@@ -5,7 +5,7 @@ public class Child01 extends Parent {
 	int m3 = 200;
 
 	void m4() {
-		System.out.println("This is a method from child class");
+		System.out.println("This is a method from Child01 class");
 	}
 
 }
---
+++ src/day24/Child02.java
@@ -0,0 +1,11 @@
+package day24;
+
+public class Child02 extends Parent {
+
+	int m5 = 300;
+
+	void m6() {
+		System.out.println("This is a method from Child02 class");
+	}
+
+}
