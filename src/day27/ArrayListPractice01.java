275e8916cfb8aafad3656c95d6edaba36cecb2b2
ShreyB <shreypractice.github@gmail.com>
2026-04-24 23:52:58 +0530

    Updates_2404_23:53

---
+++ src/day27/ArrayListPractice01.java
@@ -0,0 +1,35 @@
+package day27;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListPractice01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Object> al1 = new ArrayList<Object>();
+		System.out.println("Enter a number: ");
+		int a1 = sc.nextInt();
+		System.out.println("Enter a decimal: ");
+		double a2 = sc.nextDouble();
+		System.out.println("Enter a String: ");
+		String a3 = sc.next();
+		System.out.println("Enter a number: ");
+		int a4 = sc.nextInt();
+		System.out.println("Enter a boolean: ");
+		boolean a5 = sc.nextBoolean();
+
+		al1.add(a1);
+		al1.add(a2);
+		al1.add(a3);
+		al1.add(a4);
+		al1.add(a5);
+		al1.add('A');
+		al1.add(null);
+		al1.add(null);
+
+		System.out.println("Priting data from ArrayList: " + al1);
+
+	}
+
+}
