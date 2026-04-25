320408629c7bd2f0020b9a288c4495efb02dc11b
ShreyB <shreypractice.github@gmail.com>
2026-04-25 18:01:26 +0530

    Updates_2504_18:02

---
+++ src/day27/ArrayListPractice8.java
@@ -0,0 +1,32 @@
+package day27;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListPractice8 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Object> al1 = new ArrayList<Object>();
+
+		al1.add(150);
+		al1.add(22.7);
+		al1.add(true);
+		al1.add("Hello");
+		al1.add('S');
+		al1.add(150);
+		al1.add(null);
+		al1.add(null);
+
+		System.out.println("Printing data from ArrayList: ");
+
+		for (Object i : al1) {
+			System.out.println(i);
+		}
+
+		sc.close();
+
+	}
+
+}
