686d510a5c2e349194f152f7e1e46229a72c63b6
ShreyB <shreypractice.github@gmail.com>
2026-04-27 13:36:07 +0530

    Updates_2704_13:37

---
+++ src/day28/HashSetPractice10.java
@@ -0,0 +1,35 @@
+package day28;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HashSetPractice10 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		HashSet<Object> hs1 = new HashSet<Object>();
+
+		hs1.add(150);
+		hs1.add(22.7);
+		hs1.add(true);
+		hs1.add("Hello");
+		hs1.add('S');
+		hs1.add(150);
+		hs1.add(null);
+		hs1.add(null);
+
+		System.out.println("Printing data from HashSet before removing the element: " + hs1);
+		System.out.println("Whether the list is empty or not still?: " + hs1.isEmpty());
+
+		hs1.clear();
+
+		System.out.println("Whether the list is empty or not still?: " + hs1.isEmpty());
+
+		System.out.println("Printing data from ArrayList after removing the element: " + hs1);
+
+		sc.close();
+
+	}
+
+}
