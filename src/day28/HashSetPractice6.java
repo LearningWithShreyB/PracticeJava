87be1d52810bf6ddcd754934d4b30fb55f26d4af
ShreyB <shreypractice.github@gmail.com>
2026-04-27 13:30:46 +0530

    Updates_2704_13:31

---
+++ src/day28/HashSetPractice6.java
@@ -0,0 +1,32 @@
+package day28;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class HashSetPractice6 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		HashSet<Object> hs1 = new HashSet<Object>();
+
+		hs1.add(150);
+		hs1.add(22.7);
+		hs1.add(true);
+		hs1.add("Hello");
+		hs1.add('S');
+		hs1.add(150);
+		hs1.add(null);
+		hs1.add(null);
+
+		System.out.println("Printing data from ArrayList: ");
+
+		for (Object i : hs1) {
+			System.out.println(i);
+		}
+		sc.close();
+
+	}
+
+}
