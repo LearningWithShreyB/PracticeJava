e8102572d58e641813a621baa855c567add75434
ShreyB <shreypractice.github@gmail.com>
2026-04-27 13:15:53 +0530

    Updates_2704_13:16

---
+++ src/day28/HashSetPractice3.java
@@ -0,0 +1,35 @@
+package day28;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HashSetPractice3 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Object> al1 = new ArrayList<Object>();
+
+		al1.add(150);
+		al1.add(22.7);
+		al1.add(true);
+		al1.add("Hello");
+		al1.add('S');
+		al1.add(150);
+		al1.add(null);
+		al1.add(null);
+
+		System.out.println("Printing data from ArrayList before removing the element: " + al1);
+
+		System.out.println("Give the index number from which we have to remove the element: ");
+		int num = sc.nextInt();
+
+		al1.remove(num);
+
+		System.out.println("Printing data from ArrayList after removing the element: " + al1);
+		
+		sc.close();
+
+	}
+
+}
