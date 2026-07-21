e1237a45112ed075f1f1ca01139f001c68128992
ShreyB <shreypractice.github@gmail.com>
2026-07-21 16:52:30 +0530

    Updates_2107_16:53

---
+++ src/day30/ReverseString_2107_1.java
@@ -0,0 +1,24 @@
+package day30;
+
+import java.util.Scanner;
+
+public class ReverseString_2107_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter a string:");
+		String str=sc.next();
+		String rev="";
+		
+		for(int i=str.length()-1;i>=0;i--) {
+			rev+=str.charAt(i);
+		}
+		
+		System.out.println("The reverse string is: "+rev);
+		
+		sc.close();
+	}
+
+}
