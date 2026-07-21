c41a657bfc82939fbbfc06fb17296847c16df578
ShreyB <shreypractice.github@gmail.com>
2026-07-21 18:01:36 +0530

    Updates_2107_18:02

---
+++ src/day30/RemoveDuplicateCharacters_2107_6.java
@@ -0,0 +1,31 @@
+package day30;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RemoveDuplicateCharacters_2107_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a string:");
+		String str = sc.next();
+
+		Set<Character> set = new LinkedHashSet<Character>();
+
+		for (char ch : str.toCharArray()) {
+			set.add(ch);
+		}
+
+		System.out.println("The result is: ");
+		for (char ch : set) {
+			System.out.println(ch);
+		}
+
+		sc.close();
+	}
+
+}
