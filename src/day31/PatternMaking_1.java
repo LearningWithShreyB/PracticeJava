d058b16120b43a468705b014526855f50a2443fb
ShreyB <shreypractice.github@gmail.com>
2026-08-02 18:48:23 +0530

    Updates_0208_18:50

---
+++ src/day31/PatternMaking_1.java
@@ -0,0 +1,19 @@
+package day31;
+
+public class PatternMaking_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("The result is: ");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
